94b21ec2d283a4c22cbc91ce5426d1f479ebd0f1
Chris Fox <chris@cadnoconsulting.com>
2021-12-27 10:03:17 +0000

    Distancecalcs can use Sector Distance

--- src/calcs/DistanceCalcsTS.ts
+++ src/calcs/DistanceCalcsTS.ts
@@ -51,13 +51,13 @@ export class DistanceCalcs {
         //console.log(`HC ${handicap}, adjHC ${adjHandicap.toFixed(1)} adjDist ${adjDist.toFixed(1)}`)
         return adjDist;
     }
-    static handicappedDistance(tm:TaskModel, wind: IWind = {winddirection:0, windstrength:0}, handicap:number=100): number {
+    static handicappedDistance(tm:TaskModel, wind: IWind = {winddirection:0, windstrength:0}, handicap:number=100, useSectorDistance=false): number {
                 // code for per-leg handicapping by wind
             let dist = 0;
             let lastindex = tm.turnpoints.length-1;
             
             for (let leg=1; leg<lastindex; leg++) {
-                dist += this.legHCdistance(tm, leg, tm.turnpoints[leg].legDistance ?? 0, wind, handicap)
+                dist += this.legHCdistance(tm, leg, useSectorDistance ? (tm.turnpoints[leg].sectorDistance ?? 0) :(tm.turnpoints[leg].legDistance ?? 0), wind, handicap)
             }
             return dist;
     }
